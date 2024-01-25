2c36dc7bf70f189a763e334b8302dffa126cdc5e
Alejandro López Domínguez <alejandro.lopez@gabitel.net>
2024-01-25 08:21:46 +0100

    single embed routes for the non api stuff

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,5 @@
 import { Hono } from 'hono'
-import { getPost } from './routes/getPost';
-import { getPostLang } from './routes/getPostLang';
-import { getCollection } from './routes/getCollection';
-import { getCollectionLang } from './routes/getCollectionLang';
-import { getTag } from './routes/getTag';
-import { getTagLang } from './routes/getTagLang';
-import { getOc } from './routes/getOc';
-import { getOcLang } from './routes/getOcLang';
-import { getTagOrCollection } from './routes/getTagOrCollection';
-import { getTagOrCollectionLang } from './routes/getTagOrCollectionLang';
+import { getCollection, getOc, getPost, getTag, getTagOrCollection } from './routes/EmbedRoutes';
 import { getEmojis } from './routes/getEmojis';
 
 const app = new Hono()
@@ -17,36 +8,19 @@ app.get('/', async (c) => {
   return c.redirect('https://teslasp2.com')
 })
 
-//#region Posts
+//#region For the Embeds
 app.get("/p/:post", getPost);
 app.get("/archive/post/:post", getPost);
 
-app.get("/p/:post/:lang", getPostLang);
-app.get("/archive/post/:post/:lang", getPostLang);
-//#endregion Posts
-
-//#region OCs
 app.get("/oc/:cat/:char", getOc);
 app.get("/oc-bios/:cat/:char", getOc);
 
-app.get("/oc/:cat/:char/:lang", getOcLang);
-app.get("/oc-bios/:cat/:char/:lang", getOcLang);
-//#endregion OCs
-
-//#region Collections
 app.get("/c/:collection", getCollection);
-app.get("/c/:collection/:lang", getCollectionLang);
-//#endregion Collections
 
-//#region Tags
 app.get("/t/:tag", getTag);
-app.get("/t/:tag/:lang", getTagLang);
-//#endregion Tags
 
-//#region Col or Tag
 app.get("/archive/:tagCol", getTagOrCollection);
-app.get("/archive/:tagCol/:lang", getTagOrCollectionLang);
-//#endregion Col or Tag
+//#endregion For the Embeds
 
 app.get("/emojis", getEmojis);
 
---
+++ src/routes/EmbedRoutes.tsx
@@ -0,0 +1,107 @@
+import { Handler } from "hono";
+import { isUUID } from "validator";
+
+import { Post } from "../components/Post";
+import { Tag } from "../components/Tag";
+import { Collection } from "../components/Collection";
+import { Oc } from "../components/Oc";
+
+import { fetchPost } from "../lib/fetchPost";
+import { fetchTag } from "../lib/fetchTag";
+import { fetchCollection } from "../lib/fetchCollection";
+import { fetchOc } from "../lib/fetchOc";
+
+export const getCollection: Handler <
+    {},
+  "/c/:collection"
+> = async (c) => {
+    const { collection } = c.req.param();
+    const lang = c.req.query('lang');
+    const data = await fetchCollection(collection, lang??"en");
+
+    return c.html(
+        <Collection
+          collection={data}
+          url={`/archive/${collection}`}
+        />
+      );
+}
+
+export const getOc: Handler <
+    {},
+  "/oc/:cat/:char" | "/https://teslasp2.com/oc-bios/:cat/:char"
+> = async (c) => {
+    const { cat, char } = c.req.param();
+    const lang = c.req.query('lang');
+    const data = await fetchOc(char, lang??"en");
+
+    return c.html(
+        <Oc
+          oc={data}
+          url={`/oc-bios/${cat}/${char}`}
+        />
+      );
+}
+
+export const getPost: Handler<
+  {},
+  "/p/:post" | "/https://teslasp2.com/archive/post/:post"
+> = async (c) => {
+  const { post } = c.req.param();
+  const lang = c.req.query('lang');
+  const data = await fetchPost(post, lang??"en");
+
+  return c.html(
+    <Post
+      post={data}
+      url={data.externalLink != undefined ? data.externalLink : `/archive/post/${post}`}
+    />
+  );
+}
+
+export const getTag: Handler <
+    {},
+  "/t/:tag"
+> = async (c) => {
+    const { tag } = c.req.param();
+    const lang = c.req.query('lang');
+    const data = await fetchTag(tag, lang??"en");
+
+    return c.html(
+        <Tag
+          tag={data}
+          url={`/archive/${tag}`}
+        />
+      );
+}
+
+export const getTagOrCollection: Handler <
+  {},
+  "/archive/:tagCol"
+> = async (c) => {
+  const { tagCol } = c.req.param();
+  const lang = c.req.query('lang');
+  let isCollection = isUUID(tagCol, 4);
+  if(isCollection)
+  {
+    const data = await fetchCollection(tagCol, lang??"en");
+
+    return c.html(
+        <Collection
+          collection={data}
+          url={`/archive/${tagCol}`}
+        />
+      );
+  }
+  else
+  {
+    const data = await fetchTag(tagCol, lang??"en");
+  
+    return c.html(
+        <Tag
+          tag={data}
+          url={`/archive/${tagCol}`}
+        />
+      );
+  }
+}
