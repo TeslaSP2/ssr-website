4216f5bb16a1d5a8731a75483e13083c916a1428
TeslaSP2 <soloparainformatica@gmail.com>
2024-01-26 17:07:48 +0100

    fix abos files

--- src/lib/api/abos/fetchFiles.ts
+++ src/lib/api/abos/fetchFiles.ts
@@ -3,7 +3,7 @@ import { read } from "../../utils/Dependency";
 
 export async function fetchFiles() {
     let files: {Id:string; File:AbosFiles}[] = [];
-    let filesRef = await read<{ Id: string }[]>(`abos-files.json`);
+    let filesRef = await read<{ Id: string }[]>(`abos/abos-files.json`);
     for(const fileRef of filesRef)
     {
         files.push({Id: fileRef.Id, File: await read<AbosFiles>(`abos/files/${fileRef.Id}.json`)});
